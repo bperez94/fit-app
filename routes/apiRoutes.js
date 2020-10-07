6969efe90c48bb76bddf14ec1987fb31d95eaab0
Brandon Perez <brandonp940@gmail.com>
2020-10-06 21:14:19 -0400

    fixed bugs

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,49 +1,36 @@
-const db = require("../models/workouts")
+const Workout = require("../models/workout.js")
 
 module.exports = function (app) {
 
     app.get("/api/workouts", function (req, res) {
-        db.Workout.find({})
+        Workout.find()
             .then(data => {
-                res.json(data);
+                res.json(data)
             })
             .catch(err => {
-                res.json(err);
+                res.json(err)
             })
     });
 
-    
-    app.post("/api/workouts", (req, res) => {
-        db.Workout.create(req.body)
-          .then(data => {
-            res.json(data);
-          })
-          .catch(err => {
-            res.status(400).json(err);
-          });
-      });
+    app.post("/api/workouts", function (req, res) {
+        Workout.create({})
+            .then(data => res.json(data))
+            .catch(err => {
+                console.log("err", err)
+                res.json(err)
+            })
+    });
 
-    app.put("/api/workouts/:id", (req, res) => {
-        db.Workout.findByIdAndUpdate(
-            req.params.id,
-            { $push: { exercises: req.body } },
-            { new: true }
+    app.put("/api/workouts/:id", ({ body, params }, res) => {
+        Workout.findByIdAndUpdate(
+            params.id,
+            { $push: { exercises: body } },
+            { new: true, runValidators: true }
         )
-            .then(data => {
-                res.json(data);
-            })
+            .then(data => res.json(data))
             .catch(err => {
-                res.json(err);
-            })
-    });
-    
-    app.get("/api/workouts/range", function (req, res) {
-        db.Workout.find({})
-            .then(data => {
-                res.json(data);
-            })
-            .catch(err => {
-                res.json(err);
+                console.log("err", err)
+                res.json(err)
             })
     });
 }
